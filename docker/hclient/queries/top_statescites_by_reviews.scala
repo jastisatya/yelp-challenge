e8bd2d300c2f29b80fad681813eac2a90dcf8509
levontr <levontr@gmail.com>
2017-07-28 23:53:12 +0200

    Update top_statescites_by_reviews.scala

--- docker/hclient/queries/top_statescites_by_reviews.scala
+++ docker/hclient/queries/top_statescites_by_reviews.scala
@@ -3,6 +3,5 @@ val pbusiness = sqlContext.read.parquet("/output/yelp_academic_dataset_business.
 pbusiness.registerTempTable("business")
 val preview = sqlContext.read.parquet("/output/yelp_academic_dataset_review.parquet")
 preview.registerTempTable("review")
-val allrecords = sqlContext.sql("select b.state, b.city, count(*) totalreviews from business b group by b.state, b.city order by totalreviews desc")
-  .limit(50)
+val allrecords = sqlContext.sql("select b.state, b.city, count(*) totalreviews from business b group by b.state, b.city order by totalreviews desc").limit(50)
 allrecords.show(50)
